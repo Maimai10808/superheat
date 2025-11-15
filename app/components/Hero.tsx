5cdfefba81286f365654a66780edc210bba38484
Maimai10808 <331686549@qq.com>
2025-11-15 15:26:06 +0800

    init num2

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,25 @@
+'use client'
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import { useUserStore } from "@app/store/useUserStore";
+
 
 export default function Hero() {
+  const username = useUserStore((state) => state.username);
+  const setUsername = useUserStore((state) => state.setUsername);
+
   return (
-    <section>
-        <div className="text-center text-blue-500 font-bold">
-        This is my first Web3 Project
-        </div>
+    <section className='text-center'>
+      <div>
+      <p>用户名：{username}</p>
+
+      <button onClick={() => setUsername("Mai")} className='bg-black text-white p-5'>I'm Maimai</button>
+
+      </div>
+
+       <Button>Click me</Button>
+
+
 
     </section>
   );
