c4482ef0a15d1898a454ebc3a2c80786307ad630
Maimai10808 <331686549@qq.com>
2025-11-19 13:07:08 +0800

    add test page.tsx

---
+++ app/test/page.tsx
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+
+<body class="h-screen bg-gray-100 flex items-center justify-center">
+
+
+    <div class="w-full max-w-4xl px-4">
+      <div class="flex justify-between items-center bg-white p-8 rounded shadow-md">
+
+        <div class="text-left text-xl font-semibold text-gray-700">
+          左边的内容：介绍、标题或信息
+        </div>
+
+
+        <div class="text-right text-xl font-semibold text-gray-700">
+          右边的内容：说明、联系方式或操作
+        </div>
+      </div>
+    </div>
+
+  </body>
+
+      </main>
+    </>
+  );
+}
