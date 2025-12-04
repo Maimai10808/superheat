777f631e1b7bf24f716fecd3139307208e4e63c8
Maimai10808 <331686549@qq.com>
2025-12-04 14:16:04 +0800

    Suspense Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,28 @@
-import Image from "next/image";
-import Header from "@/components/components/layout/Header"
-import Footer from "@/components/components/layout/Footer"
 import Hero from "@/components/components/Hero";
 import Section2 from "@/components/components/sections/Section2";
 import Section3 from "@/components/components/sections/Section3";
-import Section4 from "@/components/components/sections/Section4";
 import Section5 from "@/components/components/sections/Section5";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <main>
+        <Suspense>
+          <Hero />
+        </Suspense>
 
+        <Suspense>
+          <Section2 />
+        </Suspense>
 
-        <Hero />
-        <Section2 />
-        <Section3 />
-        {/* <Section4 /> */}
-        <Section5 />
+        <Suspense>
+          <Section3 />
+        </Suspense>
 
-
+        <Suspense>
+          <Section5 />
+        </Suspense>
       </main>
     </>
   );
