e7e674597afadbc05c62cdb5f076a04b4af75cd6
Maimai10808 <331686549@qq.com>
2025-12-07 18:10:23 +0800

    pokemon by React Query

---
+++ app/Pokemon/page.tsx
@@ -0,0 +1,13 @@
+import PokemonPage from "@/components/Pokemon/PokemonPage";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div className="w-full h-screen items-center justify-center">
+          <PokemonPage />
+        </div>
+      </main>
+    </>
+  );
+}
